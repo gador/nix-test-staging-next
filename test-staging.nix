b847b0f31f888e7fa7ba766d07f4791293cebb6f
Florian Brandes <florian.brandes@posteo.de>
2022-08-14 11:05:06 +0200

    update package set
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- test-staging.nix
+++ test-staging.nix
@@ -1,22 +1,19 @@
 with import ./. { };
 
 [
-  bat
   binutils
-  cabal-install
   cargo
   cmake
 
   dbus
   direnv
   enlightenment.terminology
-  exa
   fd
   git
   gitAndTools.hub
   ghc
   glances
-  gnupg                         # gpg command
+  gnupg # gpg command
   gnumake
   hicolor-icon-theme # lutris
   htop
@@ -40,10 +37,6 @@ with import ./. { };
   usbutils
   wget
 
-  # for work
-  vault
-  consul
-
   # vim plugin dependencies
   fzf
   ripgrep
@@ -51,29 +44,104 @@ with import ./. { };
   #haskell dependencies
   haskellPackages.hlint
 
-  # dotnet
-  (with dotnetCorePackages; combinePackages [
-    sdk_3_1
-    sdk_5_0
-  ])
-  chromium
-  firefox
+  brave
   discord
-  dwarf-fortress-packages.dwarf-fortress-full
-  hexchat
   jetbrains.pycharm-community
   nerdfonts
   jetbrains.rider
-  pavucontrol  # pulseaudio configuration
-  lutris
+  pavucontrol # pulseaudio configuration
   nerdfonts
   shutter # screenshots
-  spotify
   steam
   teams
   vlc
 
-  tmate
+  # from my config
+
+  unzip
+  neofetch
+
+  # Shells
+  zsh
+
+  # Processors
+  jq
+  gawk
+  gnused
+
+  # Downloaders
+  curl
+
+  # system monitors
+  acpi
+  pstree
+  iotop
+  lm_sensors
+
+  # utils
+  inxi
+  killall
+
+
+  # Nix tools
+  patchelf
+
+  # Text editor
+  nano
+
+
+  # filesystem support
+  sshfs
+  fuse
+  cifs-utils
+
+  # File manager
+  xfce.catfish
+
+  # Video conference
+  zoom-us
+  teamviewer
+  skypeforlinux
+  nomachine-client
+
+  # Productivity Suite
+  libreoffice-fresh
+  pdftk
+  vscode-fhs
+  masterpdfeditor
+  gscan2pdf
+  calibre
+  qpdfview
+
+  # tools
+  alacritty
+  gparted
+  xclip
+  borgmatic
+
+  # remote
+  vmware-horizon-client
+  remmina
+  filezilla
+  moonlight-qt
+
+  # graphics
+  gimp
+  nitrogen
+  prusa-slicer
+  digikam
+
+  # sync
+  syncthing
+  seafile-client
+
+  # social
+  element-desktop
+  golden-cheetah
+
+  # research
+  zotero
+
 
   # taken from pkgs/top-level/release.nix and nixos/release-combined.nix
   linux
@@ -124,7 +192,6 @@ with import ./. { };
   nixosTests.login
   nixosTests.misc
   nixosTests.mutableUsers
-  nixosTests.nano
   nixosTests.nat.firewall-conntrack
   nixosTests.nat.firewall
   nixosTests.nat.standalone
@@ -178,26 +245,87 @@ with import ./. { };
   jdk
 
   # packages I maintain
-  azure-cli
-  dwarf-therapist
-  pappl
-  spicetify-cli
-  yamllint
-  openrgb
-  python37Packages.psutil
-  python38Packages.psutil
-  python39Packages.psutil
-  python310Packages.psutil
-  python311Packages.psutil
 
-  python3Packages.secure
-  vimPlugins.vim-hcl
-  python3Packages.sshtunnel
+  box64
+  octoprint
+  paperless-ngx
+  pgadmin
+  pinegrow
+  python39Packages.add-trailing-comma
+  python310Packages.add-trailing-comma
+  python311Packages.add-trailing-comma
+  python39Packages.apsw
+  python310Packages.apsw
+  python311Packages.apsw
+  python39Packages.aspy-refactor-imports
+  python310Packages.aspy-refactor-imports
+  python311Packages.aspy-refactor-imports
+  python39Packages.classify-imports
+  python310Packages.classify-imports
+  python311Packages.classify-imports
+  python39Packages.deal
+  python310Packages.deal
+  python311Packages.deal
+  python39Packages.deal-solver
+  python310Packages.deal-solver
+  python311Packages.deal-solver
+  python39Packages.django-q
+  python310Packages.django-q
+  python311Packages.django-q
+  python39Packages.dnspython
+  python310Packages.dnspython
+  python311Packages.dnspython
+  python39Packages.dpcontracts
+  python310Packages.dpcontracts
+  python311Packages.dpcontracts
+  python39Packages.flask-gravatar
+  python310Packages.flask-gravatar
+  python311Packages.flask-gravatar
+  python39Packages.flask-paranoid
+  python310Packages.flask-paranoid
+  python311Packages.flask-paranoid
+  python39Packages.flask-security-too
+  python310Packages.flask-security-too
+  python311Packages.flask-security-too
+  python39Packages.httpagentparser
+  python310Packages.httpagentparser
+  python311Packages.httpagentparser
+  python39Packages.icontract
+  python310Packages.icontract
+  python311Packages.icontract
+  python39Packages.mongomock
+  python310Packages.mongomock
+  python311Packages.mongomock
+  python39Packages.pipenv-poetry-migrate
+  python310Packages.pipenv-poetry-migrate
+  python311Packages.pipenv-poetry-migrate
+  python39Packages.pyschemes
+  #python310Packages.pyschemes
+  #python311Packages.pyschemes
+  python39Packages.python-jenkins
+  python310Packages.python-jenkins
+  python311Packages.python-jenkins
+  python39Packages.pyzbar
+  python310Packages.pyzbar
+  python311Packages.pyzbar
+  python39Packages.reorder-python-imports
+  python310Packages.reorder-python-imports
+  python311Packages.reorder-python-imports
+  python39Packages.sentinels
+  python310Packages.sentinels
+  python311Packages.sentinels
+  python39Packages.speaklater3
+  python310Packages.speaklater3
+  python311Packages.speaklater3
+  python39Packages.types-redis
+  python310Packages.types-redis
+  python311Packages.types-redis
+  python39Packages.vaa
+  python310Packages.vaa
+  python311Packages.vaa
+  python39Packages.zipstream-ng
+  python310Packages.zipstream-ng
+  python311Packages.zipstream-ng
 
-  # work packages
-  nomad
-  vault-bin
-  consul
-  consul-template
 ]
 
