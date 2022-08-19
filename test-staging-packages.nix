3f1506e9502ce0673d60a64c12e99850db3d18d5
Florian Brandes <florian.brandes@posteo.de>
2022-08-19 09:56:30 +0200

    python311Packages: deactivate for now
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- test-staging-packages.nix
+++ test-staging-packages.nix
@@ -150,79 +150,79 @@ with import ./. { };
   pinegrow
   python39Packages.add-trailing-comma
   python310Packages.add-trailing-comma
-  python311Packages.add-trailing-comma
+  # python311Packages.add-trailing-comma
   python39Packages.apsw
   python310Packages.apsw
-  python311Packages.apsw
+  # python311Packages.apsw
   python39Packages.aspy-refactor-imports
   python310Packages.aspy-refactor-imports
-  python311Packages.aspy-refactor-imports
+  # python311Packages.aspy-refactor-imports
   python39Packages.classify-imports
   python310Packages.classify-imports
-  python311Packages.classify-imports
+  # python311Packages.classify-imports
   python39Packages.deal
   # python310Packages.deal
-  # python311Packages.deal
+  # # python311Packages.deal
   python39Packages.deal-solver
   python310Packages.deal-solver
-  python311Packages.deal-solver
+  # python311Packages.deal-solver
   python39Packages.django-q
   python310Packages.django-q
-  python311Packages.django-q
+  # python311Packages.django-q
   python39Packages.dnspython
   python310Packages.dnspython
-  python311Packages.dnspython
+  # python311Packages.dnspython
   python39Packages.dpcontracts
   python310Packages.dpcontracts
-  python311Packages.dpcontracts
+  # python311Packages.dpcontracts
   python39Packages.flask-gravatar
   python310Packages.flask-gravatar
-  python311Packages.flask-gravatar
+  # python311Packages.flask-gravatar
   python39Packages.flask-paranoid
   python310Packages.flask-paranoid
-  python311Packages.flask-paranoid
+  # python311Packages.flask-paranoid
   python39Packages.flask-security-too
   python310Packages.flask-security-too
-  python311Packages.flask-security-too
+  # python311Packages.flask-security-too
   python39Packages.httpagentparser
   python310Packages.httpagentparser
-  python311Packages.httpagentparser
+  # python311Packages.httpagentparser
   python39Packages.icontract
   python310Packages.icontract
-  python311Packages.icontract
+  # python311Packages.icontract
   python39Packages.mongomock
   python310Packages.mongomock
-  python311Packages.mongomock
+  # python311Packages.mongomock
   python39Packages.pipenv-poetry-migrate
   python310Packages.pipenv-poetry-migrate
-  python311Packages.pipenv-poetry-migrate
+  # python311Packages.pipenv-poetry-migrate
   python39Packages.pyschemes
   #python310Packages.pyschemes
-  #python311Packages.pyschemes
+  ## python311Packages.pyschemes
   python39Packages.python-jenkins
   python310Packages.python-jenkins
-  python311Packages.python-jenkins
+  # python311Packages.python-jenkins
   python39Packages.pyzbar
   python310Packages.pyzbar
-  python311Packages.pyzbar
+  # python311Packages.pyzbar
   python39Packages.reorder-python-imports
   python310Packages.reorder-python-imports
-  python311Packages.reorder-python-imports
+  # python311Packages.reorder-python-imports
   python39Packages.sentinels
   python310Packages.sentinels
-  python311Packages.sentinels
+  # python311Packages.sentinels
   python39Packages.speaklater3
   python310Packages.speaklater3
-  python311Packages.speaklater3
+  # python311Packages.speaklater3
   python39Packages.types-redis
   python310Packages.types-redis
-  python311Packages.types-redis
+  # python311Packages.types-redis
   python39Packages.vaa
   # python310Packages.vaa
-  # python311Packages.vaa
+  # # python311Packages.vaa
   python39Packages.zipstream-ng
   python310Packages.zipstream-ng
-  python311Packages.zipstream-ng
+  # python311Packages.zipstream-ng
 
 ]
 
